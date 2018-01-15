1d99a7df4c45c10648f63f8e42013a36c85967f1
pch <parkchyo@gmail.com>
2018-01-15 17:28:40 +0900

    first commmit

---
+++ src/main/java/com/BookStoreAdmin/BookStoreAdminApplication.java
@@ -0,0 +1,12 @@
+package com.BookStoreAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreAdminApplication.class, args);
+	}
+}
